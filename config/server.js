bb88df916d066f0fda980c980c1b69f71e3d4bf0
Sultonkhon Oblokulov <sultondeveloper@gmail.com>
2022-09-26 15:46:26 +0500

    bismillah

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '274a03c9ab300b48c817d113e40de074'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '91e44ade-fb9b-4288-831d-fe0534b60bde'
+};
